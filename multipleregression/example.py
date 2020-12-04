dca2f47a93841fdfb6e534e46c46cfbb09bbe0be
Juan Diego Reyes Ponce <jdiego.reyes@gmail.com>
2020-12-04 12:16:29 +0100

    Fixed multiple regression input data and predicting new incoming data

--- multipleregression/example.py
+++ multipleregression/example.py
@@ -1,6 +1,9 @@
 from typing import List
+import random
 
 from multipleregression.gradientdescent import least_squares_fit
+
+random.seed(0)
 
 #Multiple regression applies the same logic as simple regression, but this time 2...N independent variables are involved, working with this model instead:
 # y_i = alpha + beta1 *x_1... + betaN * x_n + epsilon
@@ -20,12 +23,27 @@ masses: List[float] = [52.21, 53.12, 54.48, 58.84, 57.20, 58.57, 61.93, 61.29, 6
 
 
 #Using gradient-descent approach, we can calculate betas:
-betas = least_squares_fit([[1 for _ in heights], heights, pregnant], masses, 0.001, 10000, 5)
+input = [[x_1, x_2, x_3] for x_1, x_2, x_3 in zip([1 for _ in heights], heights, pregnant)]
+betas = least_squares_fit(input, masses, 0.001, 5000, 25)
 
 #All-else-being-equal estimation
 print(f"Constant: {betas[0]})")
-print(f"Height: {betas[1]})") #Each additional meter corresponds to 5 more kgs / meter => Each additional cm corresponds to 50 more grams.
-print(f"Pregnant: {betas[2]})") #Being pregnant corresponds on having 4 more kilos average.
+print(f"Height: {betas[1]})") #Each additional meter corresponds to 27 more kgs / meter => Each additional cm corresponds to 200 more grams.
+print(f"Pregnant: {betas[2]})") #Being pregnant corresponds on having 4-5 more kilos average.
 
 #This model doesn't capture the interactions between variables
 #i.e. it's possible that the effect of being pregnant is different for short women and tall women
+
+#We can predict new Masses based on incoming new Heights and pregnancy. So if, based on the previous dataset, I receive:
+# height = 1,72
+# pregnant = 1
+# This function will be applied:
+# mass = 7.350997075238178 + (4.848487626011433 * 1,72) + (4.312706130154798 * 1) =
+alpha = betas[0]
+beta1 = betas[1]
+beta2 = betas[2]
+new_height = 1.72
+pregnant = 1
+new_mass = alpha + (beta1 * new_height) + (beta2 * pregnant)
+
+print(f"Multiple regression with Gradient descent for height: {new_height} and pregnant: {bool(pregnant)}: {new_mass}")
--- multipleregression/gradientdescent.py
+++ multipleregression/gradientdescent.py
@@ -23,18 +23,18 @@ def error(x: Vector, y: float, beta: Vector) -> float:
 def squared_error(x: Vector, y: float, beta: Vector) -> float:
     return error(x, y, beta) ** 2
 
-x = [1,2,3]
-y = 30
-beta = [4, 4, 4] #so prediction = 4 + 8 + 12 = 24
+# x = [1,2,3]
+# y = 30
+# beta = [4, 4, 4] #so prediction = 4 + 8 + 12 = 24
 
-assert error(x, y, beta) == -6
-assert squared_error(x, y, beta) == 36
+# assert error(x, y, beta) == -6
+# assert squared_error(x, y, beta) == 36
 
 def sqerror_gradient(x: Vector, y: float, beta: Vector) -> Vector:
     err = error(x, y, beta)
     return [2 * err * x_i for x_i in x]
 
-assert sqerror_gradient(x, y, beta) == [-12, -24, -36]
+# assert sqerror_gradient(x, y, beta) == [-12, -24, -36]
 
 def least_squares_fit(xs: List[Vector],
                       ys: List[float],
